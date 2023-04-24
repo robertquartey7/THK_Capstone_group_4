08b595390bf656a2a4b589160331fc4bd26a385c
Robert Quartey <robertquartey@Roberts-MacBook-Air.local>
2023-04-24 19:12:39 -0400

    added routes for get item and proxy

---
+++ frontend/src/utlis/dataFetch.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+// getting all store.
+export const getStore = async (url) => {
+
+    
+};
+
+// getting all items for the store.
+export const getItem = async (url) => {
+ 
+};
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+
+
+export default defineConfig(({mode})=>{
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/v1": env.VITE_BACKEND_URL || 'http://localhost:3000',
+      },
+    },
+  }
 })
+
+
