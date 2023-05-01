fbf8960475bf433527892db780284b662ddfc372
Robert Quartey <robertquartey@Roberts-MacBook-Air.local>
2023-05-01 00:14:00 -0400

    fix

--- frontend/src/utlis/protectedRoutes.js
+++ frontend/src/utlis/protectedRoutes.js
@@ -1,21 +1,15 @@
 import Cookies from "js-cookie";
-
 import { Navigate, useLocation } from "react-router-dom";
-
-
 
 
 const ProtectedRoute = ({ children }) => {
   let location = useLocation();
-console.log(location)
+  console.log(location);
 
   if (!Cookies.get("token")) {
-    
-    return <Navigate to="/login"  />;
+    return <Navigate to="/login" />;
   }
   return children;
 };
-
-
 
 export default ProtectedRoute;
